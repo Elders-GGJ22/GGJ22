d0f8e511eb1640e84f0d0fbacadd881397839474
antoniocoronato <antoniocoronato93@gmail.com>
2022-01-21 19:50:52 +0100

    Prova Scena SFX

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,27 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_CRICETO_IMPACT = 2339399990U;
+        static const AkUniqueID PLAY_CRICETO_START = 1031767396U;
         static const AkUniqueID PLAY_IMPACT_BOX = 973569650U;
         static const AkUniqueID PLAY_MAGNETISMO = 1637986760U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace CRICETO_DEATH
+        {
+            static const AkUniqueID GROUP = 2902505743U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace CRICETO_DEATH
+
+    } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
