f60ee775ca4129967b45675c7bd48863fd19d7ad
antoniocoronato <antoniocoronato93@gmail.com>
2022-01-27 13:48:41 +0100

    new SoundBank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,26 +13,53 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_BARRIERA_ELETTRICA_ACTIVE = 1366837455U;
+        static const AkUniqueID PLAY_BARRIERA_ELETTRICA_DEACTIVE = 1788800318U;
+        static const AkUniqueID PLAY_CIRCULAR_SAW = 4261671203U;
+        static const AkUniqueID PLAY_CIRCULAR_SAW_STOP = 1486119118U;
         static const AkUniqueID PLAY_EXPLOSIVE_HAMSTER_DEATH = 168179343U;
         static const AkUniqueID PLAY_GUN_NEGATIVE = 3754677826U;
         static const AkUniqueID PLAY_GUN_POSITIVE = 3140575938U;
         static const AkUniqueID PLAY_GUN_RELOAD = 2003795032U;
+        static const AkUniqueID PLAY_HAMSTER_CANNIBALE_DEATH = 1775747457U;
+        static const AkUniqueID PLAY_HAMSTER_CANNIBALE_HIT = 4187156752U;
+        static const AkUniqueID PLAY_HAMSTER_CANNIBALE_VERSO = 2681729310U;
         static const AkUniqueID PLAY_HAMSTER_DEATH = 2986090087U;
         static const AkUniqueID PLAY_HAMSTER_DEATH_BLOOD = 2465610634U;
         static const AkUniqueID PLAY_HAMSTER_DEATH_FALL = 2093550265U;
         static const AkUniqueID PLAY_HAMSTER_FOOTSTEP = 3998189881U;
+        static const AkUniqueID PLAY_HAMSTER_NINFOMANE = 91805910U;
+        static const AkUniqueID PLAY_HAMSTER_NINFOMANE_AURA = 953475988U;
+        static const AkUniqueID PLAY_HAMSTER_NINFOMANE_DEATH = 1568381641U;
+        static const AkUniqueID PLAY_HAMSTER_SAVE = 1536861472U;
         static const AkUniqueID PLAY_HAMSTER_SQUITS = 2655563858U;
-        static const AkUniqueID PLAY_HAMSTERS_SAVE = 3054066693U;
         static const AkUniqueID PLAY_NO_GUN_CHARGE = 1740526775U;
         static const AkUniqueID PLAY_OBJ_MAGNETIZED_NEGATIVE = 1224237682U;
         static const AkUniqueID PLAY_OBJ_MAGNETIZED_POSITIVE = 3344139506U;
         static const AkUniqueID PLAY_PALLINA_ALLUMINIO_IMPACT = 2353563817U;
         static const AkUniqueID PLAY_ROUTA_DESTROY = 444434914U;
         static const AkUniqueID PLAY_ROUTA_GIRA = 4128702423U;
+        static const AkUniqueID PLAY_TAGLIOLA_IMPACT = 1936083868U;
         static const AkUniqueID PLAY_TAGLIOLA_SHAKE = 592787016U;
         static const AkUniqueID PLAY_TAGLIOLA_SNAP = 928546414U;
         static const AkUniqueID STOP_OBJ_MAGNETIZED = 4178633038U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BARRIERA_ELETTRICA_ONOFF
+        {
+            static const AkUniqueID GROUP = 2603091426U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace BARRIERA_ELETTRICA_ONOFF
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
