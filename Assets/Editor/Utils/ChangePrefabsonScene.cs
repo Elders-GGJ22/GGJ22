a8e323b1d9aa54d3d8f636666cb6c41cfd7ffc73
Nicovalca <nicolavalca@hotmail.com>
2022-01-28 12:02:53 +0100

    pogo, gabbia prefab, random generator, end level2

---
+++ Assets/Editor/Utils/ChangePrefabsonScene.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scrips.Utils;
+using UnityEngine;
+using UnityEditor;
+
+
+/// <summary>Custom Editor for our PrefabSwitch script, to allow us to perform actions
+/// from the editor.</summary>
+[CustomEditor(typeof(ChangePrefabsonScene))]
+public class PrefabSwitchEditor : Editor
+{
+    /// <summary>Calls on drawing the GUI for the inspector.</summary>
+    public override void OnInspectorGUI()
+    {
+        // Draw the default inspector.
+        DrawDefaultInspector();
+
+        // Grab a reference to the target script, so we can identify it as a 
+        // PrefabSwitch, instead of a simple Object.
+        ChangePrefabsonScene prefabSwitch = (ChangePrefabsonScene)target;
+
+        // Create a Button for "Swap By Tag",
+        if(GUILayout.Button("Swap By Tag"))
+        {
+            // if it is clicked, call the SwapAllByTag method from prefabSwitch.
+            prefabSwitch.SwapAllByTag();
+        }
+
+        // Create a Button for "Swap By Array", 
+        if (GUILayout.Button("Swap By Array"))
+        {
+            // if it is clicked, call the SwapAllByArray method from prefabSwitch.
+            prefabSwitch.SwapAllByArray();
+        }
+    }
+}
